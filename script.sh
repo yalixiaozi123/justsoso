2abd447661399cca69823e3a15c1c855d95dfd1c
mauhin <71580698+mauhin@users.noreply.github.com>
2022-04-08 21:01:24 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,7 +16,7 @@ udata=${udata%\"\}\}*}
 ptopid="${udata%&*}"
 sid="${udata#*&sid=}" #登录获取ptopid和sid
 sleep 2
-curl -d "day6=b&did=1&men6=a" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o /dev/null #进入确认界面
+curl -d "day6=b&did=1&men6=a&fun18=819" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o /dev/null #进入确认界面
 sleep 2
 curl -d "@myvs.txt" -d "ptopid=$ptopid&sid=$sid" -s $url2 -o temp.txt #打卡
 udata=$(sed -n '24,26p' temp.txt)
